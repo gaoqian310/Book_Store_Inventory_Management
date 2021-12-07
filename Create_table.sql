0e31ab67a3d85b0dcbd042fc1ff282033a461c68
gaoqian310 <68983944+gaoqian310@users.noreply.github.com>
2021-12-06 22:14:53 -0700

    Update Create_table.sql

--- Create_table.sql
+++ Create_table.sql
@@ -1,3 +1,5 @@
+# Create a table named book_deatails
+
 create table book_details (
     book_name varchar2(100), 
     author_name varchar2(100), 
@@ -8,3 +10,33 @@ create table book_details (
     secondary_keyword varchar2(10), 
     rating float
     );
+
+# Insert Data to table book_details
+
+insert into book_details values ("Let us C", "Yashwant K.", "Learn C programming", 200.25, 3, "C", "software", 4.5);
+insert into book_details values ("Let us C++", "Yashwant K.", "Learn C++ programming", 230.25, 5, "C++", "C", 4.5);
+insert into book_details values ("Head first Java", "Kathy Sierra", "Learn Java programming", 330, 10, "Java", "JVM", 3.5);
+insert into book_details values ("Java: The complete reference", "Mc Graw Hill", "7th edition, learn Java programming", 783, 4, "Java", "Javabeans", 4);
+insert into book_details values ("Thinking in Java", "Bruce Eckel", "Java programming basics", 3000, 5, "Java", "JVM", 4.5);
+
+# Select only Java books
+
+select * from book_details where primary_keyword like '%java%';
+
+# Select Java books that have a rating greater than 4
+
+select * from book_details where primary_keyword like '%java%' and rating > 4;
+
+# Select books based on author name
+
+select * from book_details where author_name like '%Yashwant%';
+
+# After user pick a book for purchase, database need to update the latest count. To do this, first to get the current stock and reduce it
+
+select in_stock, book_name, price from book_details where book_name = 'Let us C';
+update book_details set in_stock = in_stock-1 where book_name = 'Let us C';
+select * from book_details where book_name = 'Let us C';
+
+# To delete books that having a rating less than 4
+
+delete from book_details where rating < 4;
